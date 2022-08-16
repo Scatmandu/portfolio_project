75b228693df09a139f8887cf1eaac7f6193cb526
Scatmandu <mcclur05@nsuok.edu>
2022-08-15 21:09:05 -0500

    added new player states and particle animations to accompany them

--- finished_product/input.js
+++ finished_product/input.js
@@ -1,12 +1,13 @@
 export class InputHandler {
-  constructor(){
+  constructor(game){
+    this.game = game;
     this.keys = [];
     window.addEventListener('keydown', e => {
       if (( e.key === 'ArrowDown' ||
             e.key === 'ArrowUp' ||
             e.key === 'ArrowLeft' ||
             e.key === 'ArrowRight' ||
-            e.key === 'Enter'
+            e.key === ' '
           ) && this.keys.indexOf(e.key) === -1){
         this.keys.push(e.key);
       } else if (e.key === 'd') this.game.debug = !this.game.debug;
@@ -16,7 +17,7 @@ export class InputHandler {
             e.key === 'ArrowUp' ||
             e.key === 'ArrowLeft' ||
             e.key === 'ArrowRight' ||
-            e.key === 'Enter'){
+            e.key === ' '){
           this.keys.splice(this.keys.indexOf(e.key), 1);
       }
     });
--- finished_product/player.js
+++ finished_product/player.js
@@ -1,4 +1,4 @@
-import { Sitting, Running, Jumping, Falling } from './playerStates.js';
+import { Sitting, Running, Jumping, Falling, Rolling, Diving } from './playerStates.js';
 
 export class Player {
   constructor(game){
@@ -18,25 +18,30 @@ export class Player {
     this.frameTimer = 0;
     this.speed = 0;
     this.maxSpeed = 10;
-    this.states = [new Sitting(this), new Running(this), new Jumping(this), new Falling(this)];
-    this.currentState = this.states[0];
-    this.currentState.enter();
+    this.states = [new Sitting(this.game),
+                   new Running(this.game),
+                   new Jumping(this.game),
+                   new Falling(this.game),
+                   new Rolling(this.game),
+                   new Diving(this.game)];
   }
   update(input, deltaTime){
+    this.checkCollision();
     this.currentState.handleInput(input);
-    // all this plus onGround function here:
-    // https://youtu.be/GFO_txvwK_c?t=26943
     // horizontal movement
     this.x += this.speed;
     if (input.includes('ArrowRight')) this.speed = this.maxSpeed;
     else if (input.includes('ArrowLeft')) this.speed = -this.maxSpeed;
     else this.speed = 0;
+    // horizontal boundaries
     if (this.x < 0) this.x = 0;
     if (this.x > this.game.width - this.width) this.x = this.game.width - this.width;
     // vertical movement
     this.y += this.vy;
     if (!this.onGround()) this.vy += this.weight;
     else this.vy = 0;
+    // vertical boundaries
+    if (this.y > this.game.height - this.height - this.game.groundMargin) this.y = this.game.height - this.height - this.game.groundMargin;
     // sprite animation
     if (this.frameTimer > this.frameInterval){
       this.frameTimer = 0;
@@ -47,6 +52,7 @@ export class Player {
     }
   }
   draw(context){
+    if (this.game.debug) context.strokeRect(this.x, this.y, this.width, this.height);
     context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
   }
   onGround(){
@@ -56,5 +62,18 @@ export class Player {
     this.currentState = this.states[state];
     this.game.speed = this.game.maxSpeed * speed;
     this.currentState.enter();
+  }
+  checkCollision(){
+    this.game.enemies.forEach(enemy => {
+      if (enemy.x < this.x + this.width &&
+          enemy.x + enemy.width > this.x &&
+          enemy.y < this.y + this.height &&
+          enemy.y + enemy.height > this.y){
+            enemy.markedForDeletion = true;
+            this.game.score++;
+      } else {
+
+      }
+    })
   }
 }
